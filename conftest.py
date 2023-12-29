535928defb28db92764ddddc93d2cd50875ff30d
Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>
2023-12-29 16:30:40 -0300

    chore(conftest): implement initial log summary and per test file

--- conftest.py
+++ conftest.py
@@ -5,6 +5,8 @@ import pytest
 from pytest import Item
 from slugify import slugify
 
+log_fullpath = pytest.StashKey[Path]()
+
 
 @pytest.hookimpl(hookwrapper=True, tryfirst=True)
 def pytest_runtest_setup(item: Item):
@@ -29,7 +31,49 @@ def pytest_runtest_setup(item: Item):
         path_args.append(item_path_name)
     path_args.append(f"{item_name}.log")
 
-    full_fname = Path(*path_args)
-    logging_plugin.set_log_path(full_fname)  # pyright: ignore[reportOptionalMemberAccess]
+    file_path = Path(*path_args)
+    logging_plugin.set_log_path(file_path)  # pyright: ignore[reportOptionalMemberAccess]
+    item.stash[log_fullpath] = file_path
     # end of create a logfile per test case
     yield
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item: Item):
+    outcome = yield
+    result = outcome.get_result()
+
+    item_logfile_path = item.stash[log_fullpath]
+    if item_logfile_path.exists():
+        file = item_logfile_path.resolve()
+
+        if result.skipped:
+            content = file.read_text()
+            if not content:
+                item_logfile_path.unlink(missing_ok=True)
+
+        if result.longrepr and not result.skipped:
+            with open(file, "a") as f:
+                f.write(f"{result.longreprtext}\n")
+
+    results_path = Path(item_logfile_path.cwd(), "results")
+    summary_path = Path(results_path, "summary.log")
+    summary_filepath = summary_path.resolve()
+
+    if result.when == "setup":
+        mode = "a" if summary_path.exists() else "w"
+        with open(summary_filepath, mode) as f:
+            f.write(f"SETUP:::{item.nodeid}:::{result.outcome.upper()}\n")
+
+            if result.failed:
+                f.write(f"\tTEST:::{item.nodeid}:::{result.outcome.upper()}\n")
+
+    if result.when == "call" or result.skipped:
+        mode = "a" if summary_path.exists() else "w"
+        with open(summary_filepath, mode) as f:
+            f.write(f"\tTEST:::{item.nodeid}:::{result.outcome.upper()}\n")
+
+    if result.when == "teardown":
+        mode = "a" if summary_path.exists() else "w"
+        with open(summary_filepath, mode) as f:
+            f.write(f"TEARDOWN:::{item.nodeid}:::{result.outcome.upper()}\n")
