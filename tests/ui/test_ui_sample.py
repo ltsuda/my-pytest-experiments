9dc06e6f7ff12cc720670cde3cd863661af1c584
Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>
2023-12-30 03:34:37 -0300

    chore(test): align pytest logging and implement new sample test

---
+++ tests/ui/test_ui_sample.py
@@ -0,0 +1,14 @@
+def test_ui_one(logger):
+    logger.info("hello world")
+    logger.info("hello world")
+    logger.debug("hello world")
+    logger.info("hello world")
+    logger.info("hello world")
+    logger.warning("hello world")
+    logger.info("hello world")
+    logger.info("hello world")
+    logger.error("hello world")
+    logger.info("hello world")
+    logger.info("hello world")
+    logger.critical("hello world")
+    assert True
