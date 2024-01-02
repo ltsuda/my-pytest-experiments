95863b12632f8436769887e10d780c5fcc97a7ae
Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>
2024-01-01 23:46:32 -0300

    chore(conftest): improve pytest hooks for logs
    This commit improves the pytest hooks to create a unique datetime for
    the test execution session because before it was recreating for each
    test case and if session took more than 1 minute, a new log directory
    were created. Also, improve hooks to only creates logfile if skip
    mark is not present and to remove any logfile related to a test case
    that doesn't have any logging call, which means, file is empty.

--- conftest.py
+++ conftest.py
@@ -3,19 +3,21 @@ import uuid
 from pathlib import Path
 
 import pytest
-from pytest import Item
+from pytest import Item, Session
 from slugify import slugify
 
 log_fullpath = pytest.StashKey[Path]()
+session_datetime = pytest.StashKey[str]()
 RESULTS_PATH = "results"
 
 
-def prepare_logfile_path(item: Item, path: list[str]) -> Path:
+def prepare_logfile_path(item: Item, *, path: list[str], session_datetime: str) -> Path:
     """Create logfile full path per test invocation <item>
 
     Args:
         item (Item): pytest Item
         path (list[str]): list of names to create file structure
+        session_datetime (str): pytest session formatted datetime
 
     Returns:
         Path: full logfile path
@@ -24,10 +26,7 @@ def prepare_logfile_path(item: Item, path: list[str]) -> Path:
     item_name = slugify(item.name)
     path_args = path.copy()
 
-    date_formatted = datetime.datetime.today().strftime("%d-%m-%Y %H:%M:%S")
-    date_formatted = slugify(date_formatted)
-
-    path_args.extend([date_formatted, "logs"])
+    path_args.extend([session_datetime, "logs"])
 
     if item.path:
         item_path_name = slugify(item.path.name.removesuffix(".py"))
@@ -44,6 +43,17 @@ def prepare_logfile_path(item: Item, path: list[str]) -> Path:
     return full_filepath
 
 
+@pytest.hookimpl(tryfirst=True)
+def pytest_runtestloop(session: Session):
+    """Pytest hook called for performing the main runtest loop (after collection finished).
+
+    Args:
+        session (Session): test execution Session
+    """
+    date_formatted = slugify(datetime.datetime.today().strftime("%d-%m-%Y %H:%M:%S"))
+    session.stash[session_datetime] = date_formatted
+
+
 @pytest.hookimpl(hookwrapper=True, tryfirst=True)
 def pytest_runtest_setup(item: Item):
     """Pytest test setup hook
@@ -52,14 +62,17 @@ def pytest_runtest_setup(item: Item):
         item (Item): test invocation item
     """
     config = item.config
+    skip_mark = item.get_closest_marker("skip")
 
-    # create logfile per test case
-    # https://docs.pytest.org/en/stable/how-to/logging.html#live-logs
-    file_path = prepare_logfile_path(item, path=[RESULTS_PATH])
-    logging_plugin = config.pluginmanager.get_plugin("logging-plugin")
-    logging_plugin.set_log_path(file_path)  # pyright: ignore[reportOptionalMemberAccess]
-    item.stash[log_fullpath] = file_path
-    # end create logfile per test case
+    if not skip_mark:
+        # create logfile per test case
+        # https://docs.pytest.org/en/stable/how-to/logging.html#live-logs
+        session_date = item.session.stash[session_datetime]
+        file_path = prepare_logfile_path(item, path=[RESULTS_PATH], session_datetime=session_date)
+        logging_plugin = config.pluginmanager.get_plugin("logging-plugin")
+        logging_plugin.set_log_path(file_path)  # pyright: ignore[reportOptionalMemberAccess]
+        item.stash[log_fullpath] = file_path
+        # end create logfile per test case
 
     yield
 
@@ -96,39 +109,45 @@ def pytest_runtest_makereport(item: Item):
     """
     outcome = yield
     result = outcome.get_result()
+    skip_mark = item.get_closest_marker("skip")
 
-    item_logfile_path: Path = item.stash[log_fullpath]
-    remove_empty_logfile_dir(item_logfile_path)
+    if not skip_mark:
+        item_logfile_path: Path = item.stash[log_fullpath]
 
-    if item_logfile_path.exists():
-        full_filepath = item_logfile_path.resolve()
+        if item_logfile_path.exists():
+            full_filepath = item_logfile_path.resolve()
 
-        if result.skipped:
-            remove_empty_logfile(full_filepath)
+            if result.skipped:
+                remove_empty_logfile(full_filepath)
 
-        if result.longrepr and not result.skipped:
-            # append failed message representation like AssertionError to respective test log file
-            with open(full_filepath, "a+") as f:
-                f.write(f"{result.longreprtext}\n")
+            if result.longrepr and not result.skipped:
+                # append failed message representation like AssertionError to respective test log file
+                with open(full_filepath, "a+") as f:
+                    f.write(f"{result.longreprtext}\n")
 
-    # create summary.log with setup/test/teardown results for all executed test cases
-    result_logs_path = item_logfile_path.parent.parent
-    summary_path = Path(result_logs_path, "summary.log")
-    summary_filepath = summary_path.resolve()
+        # create summary.log with setup/test/teardown results for all executed test cases
+        result_logs_path = item_logfile_path.parent.parent
+        summary_path = Path(result_logs_path, "summary.log")
+        summary_filepath = summary_path.resolve()
 
-    summary_message = f"{item.nodeid}:::{result.outcome.upper()}\n"
-    if result.when == "setup":
-        with open(summary_filepath, "a+") as f:
-            f.write(f"SETUP:::{summary_message}")
+        summary_message = f"{item.nodeid}:::{result.outcome.upper()}\n"
+        if result.when == "setup":
+            with open(summary_filepath, "a+") as f:
+                f.write(f"SETUP:::{summary_message}")
 
-            if result.failed:
+                if result.failed:
+                    f.write(f"TEST:::{summary_message}")
+
+        if result.when == "call" or result.skipped:
+            with open(summary_filepath, "a+") as f:
                 f.write(f"TEST:::{summary_message}")
 
-    if result.when == "call" or result.skipped:
-        with open(summary_filepath, "a+") as f:
-            f.write(f"TEST:::{summary_message}")
+        if result.when == "teardown":
+            with open(summary_filepath, "a+") as f:
+                f.write(f"TEARDOWN:::{summary_message}\n")
+            # remove empty log file at the end of test teardown
+            # example: if test doesn't have any logging call
+            remove_empty_logfile(item_logfile_path)
+            remove_empty_logfile_dir(item_logfile_path)
 
-    if result.when == "teardown":
-        with open(summary_filepath, "a+") as f:
-            f.write(f"TEARDOWN:::{summary_message}\n")
-    # end create summary.log with setup/test/teardown results for all executed test cases
+        # end create summary.log with setup/test/teardown results for all executed test cases
